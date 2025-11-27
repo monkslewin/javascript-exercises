bd00073f1f2da61426456ac3c27a8ac204eaf2d1
Finn Monks-Lewin <finnml04@icloud.com>
2025-11-27 11:33:12 +1000

    remove from array completed

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = (arr, num) => {
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] == num) {
+            arr.splice(i, 1);
+        }
+    }
+
+    return arr;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
