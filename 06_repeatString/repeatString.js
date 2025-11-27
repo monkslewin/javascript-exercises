d50ee92cdea96015246f10b5f15ab7e2d3700638
Finn Monks-Lewin <finnml04@icloud.com>
2025-11-27 11:26:34 +1000

    finished exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = (str, num) => {
+    if (num < 0) {
+        return 'ERROR';
+    };
 
-};
+    if (num == 0) {
+        return '';
+    };
 
+    repeatedString = '';
+
+    for (i = 0; i < num; i++) {
+        repeatedString += str;
+    }
+
+    return repeatedString;
+}
+
+console.log(repeatString('hello', 10));
 // Do not edit below this line
 module.exports = repeatString;
