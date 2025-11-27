398e67d49dc707a6cb19b9f9f52b1f35a15a8f7a
Finn Monks-Lewin <finnml04@icloud.com>
2025-11-27 11:28:41 +1000

    finished exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = (str) => {
+    return str
+        .split("")
+        .reverse()
+        .join("");
+}
 
-};
+console.log(reverseString("Hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
